7c5e7ce87ac851316667f177d7c6d5cb10c06cfb
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-03-16 17:19:52 -0400

    added eslint and prettier settings

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+
+const routes = require('./routes');
 
 const app = express();
 
 // define port
 const port = 3000;
 
-//middleware 
+// set up EJS template framework
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+//  static middleware
 app.use(express.static(path.join(__dirname, './static')));
 
-//serve a static file
-app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, './static/index.html'));
-});
-app.get('/speakers', (request, response) => {
-    response.sendFile(path.join(__dirname, './static/speakers.html'));
-});
+app.use('/', routes());
 
+app.get('/speakers', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
+});
 
 app.listen(port, () => {
-    console.log(`Express server listening on port ${port}`);
+  console.log(`Express server listening on port ${port}`);
 });
