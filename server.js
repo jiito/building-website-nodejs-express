b632174c1164f57c7dd2a3fe62b43a5af588d9ce
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-03-16 14:49:12 -0400

    use middleware for static files

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ const app = express();
 // define port
 const port = 3000;
 
+//middleware 
+app.use(express.static(path.join(__dirname, './static')));
+
 //serve a static file
 app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, './static/index.html'));
 });
+app.get('/speakers', (request, response) => {
+    response.sendFile(path.join(__dirname, './static/speakers.html'));
+});
+
 
 app.listen(port, () => {
     console.log(`Express server listening on port ${port}`);
