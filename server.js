6a8b27215968d67c8f5bee9b5efd677c867c3c06
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-03-17 14:39:15 -0400

    added middlewares and services

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakerService = new SpeakerService('./data/speakers.json');
 
 const routes = require('./routes');
 
@@ -8,14 +15,41 @@ const app = express();
 // define port
 const port = 3000;
 
+// cookies
+app.set('trust proxy', 1);
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['hdsjkslahj2373897', 'asjdfhashjfls223'],
+  })
+);
+
 // set up EJS template framework
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
+app.locals.siteName = 'ROUX Meetups';
+
 //  static middleware
 app.use(express.static(path.join(__dirname, './static')));
 
-app.use('/', routes());
+app.use(async (request, response, next) => {
+  try {
+    const names = await speakerService.getNames();
+    response.locals.speakerNames = names;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakerService,
+  })
+);
 
 app.get('/speakers', (request, response) => {
   response.sendFile(path.join(__dirname, './static/speakers.html'));
