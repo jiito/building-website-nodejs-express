edd2788a5f2c4bca0919e52f4aac5c81267cb602
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-03-17 14:39:32 -0400

    inital commit

---
+++ routes/feedback.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+
+module.exports = params => {
+  const { feedbackService } = params;
+  // serve a static file
+  router.get('/', async (request, response) => {
+    const feedback = await feedbackService.getList();
+    return response.json(feedback);
+  });
+
+  router.post('/', (request, response) => {
+    return response.send('Feedback form posted');
+  });
+
+  return router;
+};
